b2fe8ffac228e44955055984b2efb2e5a294ac98
E-Vister <blitz.vister@gmail.com>
2023-01-26 01:13:14 +0300

    Initial commit

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/Header.module.scss';
+
+const Header = () => {
+    return (
+        <header className={styles.container}>Header</header>
+    )
+}
+
+export default Header;
