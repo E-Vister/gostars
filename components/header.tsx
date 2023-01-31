debca8a88957bc71db257b1bec3908f9fdc81983
Yahor Bausiuk <visterovegor@gmail.com>
2023-01-31 17:00:48 +0300

    Add results, matches layouts, header buttons

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,20 @@
 import styles from '@/styles/Header.module.scss';
+import Link from 'next/link';
 
 const Header = () => {
     return (
-        <header className={styles.container}>Header</header>
+        <header className={styles.container}>
+            <div className={styles.logo}>
+                <Link href={'/'}>Header</Link>
+            </div>
+            <Link href={'/matches'}>
+                <button className={styles.button} role="link">Matches</button>
+            </Link>
+            <Link href={'/results'}>
+                <button className={styles.button} role="link">Results</button>
+            </Link>
+
+        </header>
     )
 }
 
--- pages/matches.tsx
+++ pages/matches.tsx
@@ -1,69 +1,66 @@
 import Head from "next/head";
 import Header from "@/components/header";
+import styles from "@/styles/Matches.module.scss";
 import React from "react";
-import styles from '@/styles/Results.module.scss';
 import Image from "next/image";
-import {clsx} from 'clsx';
 
-const Results = () => {
+const Matches = () => {
     return (
         <>
             <Head>
-                <title>Results | GoStars</title>
-                <meta name="description" content="Results page of the gostars"/>
+                <title>Matches | GoStars</title>
+                <meta name="description" content="Matches page of the gostars"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <Header/>
             <main className={styles.main}>
                 <div className={styles.container}>
-                    <div className={styles.page_headline}>Results</div>
-                    <ResultsSublist/>
-                    <ResultsSublist/>
-                    <ResultsSublist/>
-                    <ResultsSublist/>
+                    <div className={styles.page_headline}>Matches</div>
+                    <MatchesSublist/>
+                    <MatchesSublist/>
+                    <MatchesSublist/>
+                    <MatchesSublist/>
                 </div>
             </main>
         </>
     )
 }
 
-const ResultsSublist = () => {
+const MatchesSublist = () => {
     return (
-        <div className={styles.results_sublist}>
-            <div className={styles.headline}>Results for January 27th 2023</div>
-            <ResultCell/>
-            <ResultCell/>
-            <ResultCell/>
+        <div className={styles.matches_sublist}>
+            <div className={styles.headline}>Tuesday - 2023-01-31</div>
+            <MatchCell/>
+            <MatchCell/>
+            <MatchCell/>
         </div>
     )
 }
 
-const ResultCell = () => {
+const MatchCell = () => {
     return (
-        <div className={styles.result}>
+        <div className={styles.match}>
             <table>
                 <tbody>
                 <tr>
-                    <TeamCell teamType={'team1'} isWon={true}/>
+                    <td className={styles.match_info}>
+                        <div className={styles.match_time}>14:00</div>
+                        <div className={styles.match_meta}>bo3</div>
+                    </td>
+                    <TeamCell teamType={'team1'}/>
                     <td className={styles.score}>
-                        <span className={styles.score_won}>2</span>
-                        <span className={styles.dash}>-</span>
-                        <span className={styles.score_lost}>1</span>
+                        <span className={styles.dash}>vs</span>
                     </td>
                     <TeamCell teamType={'team2'}/>
-                    <td className={styles.event}>
+                    <td className={styles.match_additional}>
                         <Image
                             loader={() => 'https://i.imgur.com/t1HQOz0.png'}
                             src={'https://i.imgur.com/t1HQOz0.png'}
                             alt={'BLAST Premier Spring Groups 2023'}
                             width={30}
                             height={30}/>
-                        <span className={styles.event_name}>BLAST Premier Spring Groups 2023</span>
-                    </td>
-                    <td className={styles.match_type}>
-                        <div>LAN</div>
-                        <div>bo3</div>
+                        <span className={styles.match_type}>LAN</span>
                     </td>
                 </tr>
                 </tbody>
@@ -72,13 +69,11 @@ const ResultCell = () => {
     )
 }
 
-const TeamCell = ({teamType, isWon}: any) => {
+const TeamCell = ({teamType}: any) => {
     return (
         <td className={styles.team_cell}>
             <div className={`${styles.line_align} ${styles[teamType]}`}>
-                <div className={`${styles.team} ${clsx({
-                    [styles.team_won]: isWon === true,
-                })}`}>
+                <div className={styles.team}>
                     Natus Vincere
                 </div>
                 <Image
@@ -93,4 +88,4 @@ const TeamCell = ({teamType, isWon}: any) => {
     )
 }
 
-export default Results;
+export default Matches;
